3d38c1993f0682120f3a2404197825e90427d916
Andrea <hlr@HLRs-MacBook-Pro.local>
2019-10-02 15:25:50 +0100

    Add Carthage support

---
+++ AAInfographics/AAInfographics.h
@@ -0,0 +1,19 @@
+//
+//  AAInfographics.h
+//  AAInfographics
+//
+//  Created by HLR on 02/10/2019.
+//  Copyright © 2019 An An. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AAInfographics.
+FOUNDATION_EXPORT double AAInfographicsVersionNumber;
+
+//! Project version string for AAInfographics.
+FOUNDATION_EXPORT const unsigned char AAInfographicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAInfographics/PublicHeader.h>
+
+
